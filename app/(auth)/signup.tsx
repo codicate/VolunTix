3ddf17aae91ac8cd7c1178f0ee5923fb786a2d11
Henry Liu <codicate@gmail.com>
2023-11-03 23:26:52 -0400

    improved styling for sign up sign in screen

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,7 +1,7 @@
 import { Text, View, TextInput, StyleSheet } from 'react-native';
 import { useRef } from 'react';
 import { Stack, useRouter } from 'expo-router';
-import { Button } from '@rneui/themed';
+import { Button, Input } from '@rneui/themed';
 import { appSignUp, fakeSignIn } from '#configs/store';
 
 export default function SignUp() {
@@ -23,57 +23,48 @@ export default function SignUp() {
 	};
 
 	return (
-		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+		<View style={styles.screen}>
 			<Stack.Screen options={{ title: 'Sign Up', headerLeft: () => <></> }} />
-			<View>
-				<Text style={styles.label}>First Name</Text>
-				<TextInput
-					placeholder="firstName"
-					nativeID="firstName"
-					onChangeText={(text) => {
-						firstNameRef.current = text;
-					}}
-					style={styles.textInput}
-				/>
-			</View>
-			<View>
-				<Text style={styles.label}>Last Name</Text>
-				<TextInput
-					placeholder="lastName"
-					nativeID="lastName"
-					onChangeText={(text) => {
-						lastNameRef.current = text;
-					}}
-					style={styles.textInput}
-				/>
-			</View>
-			<View>
-				<Text style={styles.label}>Email</Text>
-				<TextInput
-					placeholder="email"
-					nativeID="email"
-					onChangeText={(text) => {
-						emailRef.current = text;
-					}}
-					style={styles.textInput}
-				/>
-			</View>
-			<View>
-				<Text style={styles.label}>Password</Text>
-				<TextInput
-					placeholder="password"
-					secureTextEntry={true}
-					nativeID="password"
-					onChangeText={(text) => {
-						passwordRef.current = text;
-					}}
-					style={styles.textInput}
-				/>
-			</View>
+			<Input
+				placeholder="firstName"
+				nativeID="firstName"
+				onChangeText={(text) => {
+					firstNameRef.current = text;
+				}}
+				style={styles.textInput}
+				inputContainerStyle={{ borderBottomWidth: 0 }}
+			/>
+			<Input
+				placeholder="lastName"
+				nativeID="lastName"
+				onChangeText={(text) => {
+					lastNameRef.current = text;
+				}}
+				style={styles.textInput}
+				inputContainerStyle={{ borderBottomWidth: 0 }}
+			/>
+			<Input
+				placeholder="email"
+				nativeID="email"
+				onChangeText={(text) => {
+					emailRef.current = text;
+				}}
+				style={styles.textInput}
+				inputContainerStyle={{ borderBottomWidth: 0 }}
+			/>
+			<Input
+				placeholder="password"
+				secureTextEntry={true}
+				nativeID="password"
+				onChangeText={(text) => {
+					passwordRef.current = text;
+				}}
+				style={styles.textInput}
+				inputContainerStyle={{ borderBottomWidth: 0 }}
+			/>
 			<Button
 				title={'Sign Up'}
 				containerStyle={{
-					height: 40,
 					width: 200,
 					marginHorizontal: 50,
 					marginVertical: 10,
@@ -86,6 +77,7 @@ export default function SignUp() {
 				onPress={async () => signUp()}
 			/>
 			<Text
+				style={styles.textLink}
 				onPress={() => {
 					router.push('/signin');
 				}}
@@ -93,6 +85,7 @@ export default function SignUp() {
 				Have an account? Sign In
 			</Text>
 			<Text
+				style={styles.textLink}
 				onPress={() => {
 					fakeSignIn();
 					router.push('/events');
@@ -105,15 +98,21 @@ export default function SignUp() {
 }
 
 const styles = StyleSheet.create({
-	label: {
-		marginBottom: 4,
-		color: '#455fff',
+	screen: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: 300,
+		margin: 'auto',
+	},
+	textLink: {
+		color: '#2089dc',
 	},
 	textInput: {
 		width: 250,
 		borderWidth: 1,
 		borderRadius: 4,
-		borderColor: '#455fff',
+		borderColor: '#2089dc',
 		paddingHorizontal: 8,
 		paddingVertical: 4,
 		marginBottom: 8,
