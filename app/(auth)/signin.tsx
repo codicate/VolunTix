311841472a1580a72298b9ff348abbb7b899475e
sidnarsipur <72701664+sidnarsipur@users.noreply.github.com>
2023-11-04 19:59:24 -0400

    Sign-in Merge Conflict Resolved

--- app/(auth)/signin.tsx
+++ app/(auth)/signin.tsx
@@ -102,7 +102,7 @@ export const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     width: '100%',
-    paddingHorizontal: 25,
+    paddingHorizontal: 35,
   },
   textLink: {
     fontSize: 20,
