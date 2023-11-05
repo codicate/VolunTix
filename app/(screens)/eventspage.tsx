fd5e745a64250df764e4151d2131ab560c9f8333
sidnarsipur <72701664+sidnarsipur@users.noreply.github.com>
2023-11-05 00:52:58 -0400

    Events Page Update

--- app/(screens)/eventspage.tsx
+++ app/(screens)/eventspage.tsx
@@ -5,8 +5,9 @@ import styles from "app/stylesScreen";
 import volunteeringData from "assets/server/events.json";
 import { registerForEvent } from "#configs/userStore";
 
-import { Card, Icon } from "@rneui/themed";
+import { Card } from "@rneui/themed";
 import { AuthStore } from "#configs/authStore";
+import Icon from "react-native-vector-icons/Feather";
 
 const EventsPage = () => {
 	const router = useRouter();
@@ -28,40 +29,136 @@ const EventsPage = () => {
 
 	return (
 		<View style={{ backgroundColor: "white" }}>
-			<View style={styles.container}>
+			<View style={{ padding: 0 }}>
 				<Image
 					source={{
-						uri: "https://media.tenor.com/8dTD4BUZfS8AAAAC/%CE%B2%CE%B5%CF%81%CF%84%CE%B7%CF%82-yton.gif",
+						uri: currentVolunteering.image,
+					}}
+					style={{
+						minWidth: "100%",
+						minHeight: 10,
+						width: "100%",
+						height: "70%",
 					}}
 				></Image>
-				<Card>
-					<Card.Image
-						source={{ uri: currentVolunteering.image }}
-						style={{ ...styles.image, resizeMode: "cover" }}
-					/>
-					<View style={styles.locationPointsContainer}>
-						<Card.Title style={styles.location}>
-							{currentVolunteering.location}
-						</Card.Title>
-						<View style={styles.pointsContainer}>
-							<Icon name="star" type="font-awesome" color="#f50" />
-							<Text>{currentVolunteering.points}</Text>
+				<View
+					style={{
+						flexDirection: "column",
+						alignItems: "center",
+						marginLeft: "2.5%",
+						marginTop: "5%",
+						paddingBottom: "5%",
+						backgroundColor: "white",
+					}}
+				>
+					<Text style={{ color: "#1a237e", fontSize: 20 }}>
+						In-Person Event
+					</Text>
+					<Text style={{ color: "black", fontWeight: "bold", fontSize: 35 }}>
+						{currentVolunteering.title}
+					</Text>
+					<Text style={{ marginTop: "2.5%", color: "gray", fontSize: 20 }}>
+						By <u>Goodwill New York</u>
+					</Text>
+				</View>
+				<View>
+					<TouchableOpacity
+						style={{
+							...styles.volunteerButton,
+							backgroundColor: registered ? "#888" : "#2f95dc",
+						}}
+						onPress={handleVolunteerButtonClick}
+					>
+						<Text style={styles.buttonText}>
+							{registered ? "Registered" : "Volunteer"}
+						</Text>
+					</TouchableOpacity>
+				</View>
+				<View
+					style={{
+						flexDirection: "column",
+						marginLeft: "5%",
+						marginTop: "6%",
+						gap: 10,
+					}}
+				>
+					<View
+						style={{
+							flexDirection: "row",
+							alignItems: "center",
+						}}
+					>
+						<Icon name="calendar" size={12.5} color="black" />
+						<Text
+							style={{
+								color: "black",
+								fontSize: 20,
+								marginLeft: 10,
+							}}
+						>
+							Nov 3
+						</Text>
+					</View>
+					<View
+						style={{
+							flexDirection: "row",
+							alignItems: "center",
+						}}
+					>
+						<Icon name="map-pin" size={12.5} color="black" />
+						<Text
+							style={{
+								color: "black",
+								fontSize: 20,
+								marginLeft: 10,
+							}}
+						>
+							<u>123 Example Street</u>
+						</Text>
+					</View>
+					<View
+						style={{
+							flexDirection: "row",
+							alignItems: "center",
+							marginTop: "5%",
+						}}
+					>
+						<View
+							style={{
+								flexDirection: "column",
+							}}
+						>
+							<Text
+								style={{
+									color: "black",
+									fontWeight: "bold",
+									fontSize: 20,
+								}}
+							>
+								Event Details
+							</Text>
+							<Text>{currentVolunteering.text}</Text>
 						</View>
 					</View>
-					<Card.Divider />
-					<Text style={styles.description}>{currentVolunteering.text}</Text>
-				</Card>
-				<TouchableOpacity
-					style={{
-						...styles.volunteerButton,
-						backgroundColor: registered ? "#888" : "#2f95dc",
-					}}
-					onPress={handleVolunteerButtonClick}
-				>
-					<Text style={styles.buttonText}>
-						{registered ? "Registered" : "Volunteer"}
-					</Text>
-				</TouchableOpacity>
+					<View
+						style={{
+							marginTop: "5%",
+							flexDirection: "row",
+							alignItems: "center",
+						}}
+					>
+						<View>
+							<Icon name="plus-circle" color="blue" size={30}></Icon>
+						</View>
+						<View style={{ marginLeft: "2%" }}>
+							<Text
+								style={{ color: "black", fontSize: 15, fontWeight: "bold" }}
+							>
+								Share this event
+							</Text>
+						</View>
+					</View>
+				</View>
 			</View>
 		</View>
 	);
