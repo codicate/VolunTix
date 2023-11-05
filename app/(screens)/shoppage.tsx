37e5e3d2e71a09b2d1980d9fb2aa1171cb612de0
sidnarsipur <72701664+sidnarsipur@users.noreply.github.com>
2023-11-05 01:19:56 -0400

    Shop Page Done

--- app/(screens)/shoppage.tsx
+++ app/(screens)/shoppage.tsx
@@ -3,9 +3,10 @@ import React from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import styles from "app/stylesScreen";
 import shopData from "assets/server/shop.json";
-import { Card, Icon } from "@rneui/themed";
+import { Card } from "@rneui/themed";
 import { AuthStore } from "#configs/authStore";
 import { redeemItem } from "#configs/userStore";
+import Icon from "react-native-vector-icons/Feather";
 
 const EventsPage = () => {
 	const user = AuthStore.useState((s) => s.user);
@@ -48,26 +49,136 @@ const EventsPage = () => {
 	};
 
 	return (
-		<View>
-			<View style={styles.header}>
-				<Text style={styles.backButton} onPress={handleBackButtonClick}>
-					Back
-				</Text>
-				<Text style={styles.headerText}>{event.title}</Text>
-			</View>
-			<View style={styles.container}>
-				<Card containerStyle={styles.eventCard}>
-					<Card.Image source={{ uri: item.image }} style={styles.image} />
-					<View style={styles.locationPointsContainer}>
-						<Card.Title style={styles.location}>{item.name}</Card.Title>
-						<View style={styles.pointsContainer}>
-							<Icon name="star" type="font-awesome" color="#f50" />
-							<Text>{item.points}</Text>
+		<View style={{}}>
+			<View style={{ padding: 0 }}>
+				<Image
+					source={{
+						uri: item.image,
+					}}
+					style={{
+						minWidth: "100%",
+						minHeight: 10,
+						width: "100%",
+						height: "60%",
+					}}
+				></Image>
+				<View
+					style={{
+						flexDirection: "column",
+						alignItems: "center",
+						marginLeft: "2.5%",
+						marginTop: "5%",
+						paddingBottom: "5%",
+					}}
+				>
+					<Text style={{ color: "#1a237e", fontSize: 20 }}>Sports</Text>
+					<Text style={{ color: "black", fontWeight: "bold", fontSize: 35 }}>
+						{item.name}
+					</Text>
+					<Text style={{ marginTop: "2.5%", color: "gray", fontSize: 20 }}>
+						@ <u>Madison Square Garden</u>
+					</Text>
+				</View>
+				<View></View>
+				<View
+					style={{
+						flexDirection: "column",
+						marginLeft: "5%",
+						marginTop: "6%",
+						gap: 10,
+					}}
+				>
+					<View
+						style={{
+							flexDirection: "row",
+							alignItems: "center",
+						}}
+					>
+						<Icon name="calendar" size={12.5} color="black" />
+						<Text
+							style={{
+								color: "black",
+								fontSize: 20,
+								marginLeft: 10,
+							}}
+						>
+							Nov 3
+						</Text>
+					</View>
+					<View
+						style={{
+							flexDirection: "row",
+							alignItems: "center",
+						}}
+					>
+						<Icon name="map-pin" size={12.5} color="black" />
+						<Text
+							style={{
+								color: "black",
+								fontSize: 20,
+								marginLeft: 10,
+							}}
+						>
+							<u>123 Example Street</u>
+						</Text>
+					</View>
+					<View
+						style={{
+							flexDirection: "row",
+							alignItems: "center",
+							marginTop: "5%",
+						}}
+					>
+						<View
+							style={{
+								flexDirection: "column",
+								gap: 20,
+							}}
+						>
+							<View>
+								<Text style={{ fontSize: 20 }}>120 Points To Redeem</Text>
+							</View>
+							<View>
+								<Text
+									style={{
+										color: "black",
+										fontWeight: "bold",
+										fontSize: 20,
+									}}
+								>
+									Event Details
+								</Text>
+								<View
+									style={{
+										marginTop: "0.5%",
+										maxWidth: "25%",
+										flexWrap: "wrap",
+									}}
+								>
+									<Text style={{}}>{item.description}</Text>
+								</View>
+							</View>
 						</View>
 					</View>
-					<Card.Divider />
-					<Text style={styles.description}>{item.description}</Text>
-				</Card>
+					<View
+						style={{
+							marginTop: "3%",
+							flexDirection: "row",
+							alignItems: "center",
+						}}
+					>
+						<View>
+							<Icon name="plus-circle" color="blue" size={30}></Icon>
+						</View>
+						<View style={{ marginLeft: "2%" }}>
+							<Text
+								style={{ color: "black", fontSize: 15, fontWeight: "bold" }}
+							>
+								Share this event
+							</Text>
+						</View>
+					</View>
+				</View>
 				<TouchableOpacity
 					style={styles.volunteerButton}
 					onPress={() => handleRedeemButtonClick(item.name, item.points)}
